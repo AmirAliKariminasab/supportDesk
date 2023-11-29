a5ad51e9c617803de18dc3b3ae014c4112291f2d
AmirAliKariminasab <kariminasab.amirali1381@gmail.com>
2023-11-29 12:08:34 +0330

    Initial Commit03

--- backend/Routes/ticketRoutes.js
+++ backend/Routes/ticketRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getTickets, createTicket, getTicket, updateTicket, deleteTicket } = require('../controllers/ticketController')
+const { getTickets, createTicket, getTicket, updateTicket, deleteTicket } = require('../controllers/ticketController');
+
+// Re-route into note Router
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes', noteRouter);
 
 
 const { protect } = require('../middleware/authMiddleware');
--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -13,8 +13,44 @@ const createTicket = async (ticketData, token) => {
     return response.data;
 }
 
+// Get User tickets 
+const getTickets = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    };
+    const response = await axios.get(API_URL, config);
+    return response.data;
+}
+
+// Get User ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    };
+    const response = await axios.get(API_URL + ticketId, config);
+    return response.data;
+}
+
+// Close  ticket
+const closeTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    };
+    const response = await axios.put(API_URL + ticketId, { status: 'closed' }, config);
+    return response.data;
+}
+
 const ticketService = {
     createTicket,
+    getTickets,
+    getTicket,
+    closeTicket,
 }
 
 export default ticketService;
