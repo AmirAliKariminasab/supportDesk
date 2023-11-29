5cf49f0e061b4470de5219063bc4ef07f237e1aa
AmirAliKariminasab <kariminasab.amirali1381@gmail.com>
2023-11-29 14:45:36 +0330

    Initial Commit05

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDb = require('./config/db');
 const express = require('express');
@@ -13,13 +14,21 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello' })
-})
-
 // Routes
 app.use('/api/users', require('./Routes/userRoutes'));
 app.use('/api/tickets', require('./Routes/ticketRoutes'));
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+}
 
 app.use(errorHandler);
 
